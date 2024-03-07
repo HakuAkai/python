50e74cc78b23b6f25ea4730582c6bc973bfc2f8a
Garcia <hulijingkumiho@gmail.com>
2024-03-07 11:35:13 -0300

    Aula 04 created

---
+++ Aula01 - Aula05/Aula04/dados.py
@@ -0,0 +1,7 @@
+print("------------------------------------------")
+print("| FIAP paulista                          |")
+print("| Análise e Desenvolvimento de Sistemas  |")
+print("| Júlia Garcia Dellatorre                |")
+print("| RM555767                               |")
+print("| juhh1956@gmail.com                     |")
+print("------------------------------------------")
