8d1fec33d83b13e060e90b231a88b290cf6bda25
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-11 09:03:32 -0300

    IMC.py update

--- IMC.py
+++ IMC.py
@@ -1,15 +1,19 @@
-Altura = int(input("Digite sua altura: "))
-Peso = int(input("Digite seu peso: "))
+Altura = float(input("Digite sua altura: "))
+Peso = float(input("Digite seu peso: "))
 
 print(Altura, Peso)
 
-IMC = (Altura * Altura) / Peso
+IMC = Peso / (Altura**2) 
 
 print(IMC)
 
 if IMC > 25:
     print("Sobrepreso ou mais")
+elif IMC < 18.5:
+    print("Abaixo do peso ou menor")
 else:
-    print("Peso normal para baixo") 
+    print("Peso normal") 
+
+
 
 
