87ccf636f2b7ec950394676c5eb2e88bbe12b5c9
Garcia <hulijingkumiho@gmail.com>
2024-03-14 17:58:11 -0300

    Folder "Aula 05" created

---
+++ Aula01 - Aula05/Aula 05/testevar.py
@@ -0,0 +1,33 @@
+a = 10 #int
+b = False #boolean
+c = "Hello World" #string
+d = 17.84 #float
+
+cp1 = 8.5
+cp2 = 7.6
+cp3 = 0.0
+
+cp_list = [8.5, 7.6, 0.0]
+print("Primeiro valor da lista: ", cp_list[0])
+print("Segundo valor da lista: ", cp_list[1])
+print("Terceiro valor da lista: ", cp_list[2])
+print("Lista completa: ", cp_list)
+
+cp_list[2] = 9.0
+print("Lista atualizada: ", cp_list)
+
+print("------------------------------------------")
+valor = -300
+absoluto = abs( valor )
+print("Valor ", valor)
+print("Valor absoluto ", absoluto)
+
+print("------------------------------------------")
+j = 64
+f = float(j)
+h = hex(j)
+o = oct(j)
+print("J: ", j)
+print("F: ", f)
+print("H: ", h)
+print("O: ", o)
---
+++ Aula01 - Aula05/Aula 05/testevar2.py
@@ -0,0 +1,33 @@
+a = 79
+a_em_octal = oct( a )
+print("A em octal: ", a_em_octal)
+
+# Bases numericas
+# Algarismos Binários:  0 ou 1
+# Algarismos Octais: 0, 1, 2, 3, 4, 5, 6, 7
+# Algarismos Decimais: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+# Algarismos Hexadecimais: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, F
+
+print("------------------------------------------")
+n1 = 8170990732
+numero_em_binario = bin(n1)
+numero_em_octal = oct(n1)
+numero_em_hexadecimal = hex(n1)
+print("N1: ", n1)
+print("Numero Binario: ", numero_em_binario)
+print("Numero Octal: ", numero_em_octal)
+print("Numero Hexadecimal: ", numero_em_hexadecimal)
+
+print("------------------------------------------")
+n1      
+str(n1) 
+print("Habitantes da terra são: " + str(n1) )
+print("Habitantes da terra são: ", n1)
+
+print("------------------------------------------")
+f = 971.72424726
+i = int(f)      # 971
+s = str(i)      # "971"
+print("Numero Float: ", f)
+print("Parte Inteira: ", i)
+print("Parte Inteira: " + s)
